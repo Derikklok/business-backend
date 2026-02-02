1857394c0db4a6c1eceb5bbdcb8b0b085b7003b7
Sachin <145639140+Derikklok@users.noreply.github.com>
2026-02-02 21:44:10 +0530

    Add database connection and dotenv support; update .gitignore and package.json

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import { Elysia } from "elysia";
+import { connectDB } from "./config/db";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+// Connect to db
+await connectDB();
 
+const app = new Elysia({ prefix: "/api" });
+export default app.get("/", () => "Hello Elysia").listen(3000);
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
